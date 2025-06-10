b22b4eefcfa47b90f6257b339bb6cf4be35d6216
CesarLinux <eduardomelchor34@gmail.com>
2025-06-09 21:16:58 -0600

    Fucniones con multiples retornos

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -4,9 +4,7 @@ function greet( name: string ): string {
   return `Hola ${ name }`;
 }
 
-const greet2 = (name: string): string => {
-  return `Hola ${ name }`;
-}
+const greet2 = (name: string): string => `Hola ${ name }`;
 
 const message = greet( 'Goku' );
 const message2 = greet2( 'Vegeta' );
@@ -14,20 +12,38 @@ const message2 = greet2( 'Vegeta' );
 console.log(message, message2)
 
 
-function getUser() {
+interface User {
+  uid: string;
+  username: string;
+}
+
+
+function getUser(): User {
   return {
     uid: 'ABC-123',
     username: 'El_Papi123'
   }
 }
 
-const getUser2 = () => {
-  return {
-    uid: 'ABC-1234567',
-    username: 'El_Papi123'
-  }
-}
+const getUser2 = () => ({ 
+  uid: 'ABC-1234567', 
+  username: 'El_Papi123'
+});
 
 const user = getUser();
 const user2 = getUser2();
 console.log(user, user2);
+
+
+
+const myNumbers: number[] = [ 1,2,3,4,5 ];
+
+// myNumbers.forEach( function(value) {
+//   console.log({value});
+// });
+
+// myNumbers.forEach( (value) => {
+//   console.log(value)
+// });
+
+myNumbers.forEach( console.log );
