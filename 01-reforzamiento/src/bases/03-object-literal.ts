7b0fabbef14b8bca5b14b2b3b54e0fde83051c4a
CesarLinux <eduardomelchor34@gmail.com>
2025-06-09 20:31:48 -0600

    Interface de TypeScript

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,21 +1,48 @@
 
 
-const ironman = {
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: Address;
+}
+
+interface Address {
+  postalCode: string;
+  city: string
+}
+
+
+const ironman: Person = {
   firstName: 'Tony',
   lastName: 'Stark',
   age: 45,
   address: {
-    postalCode: 'ABC123',
+    postalCode: 'ABC-123',
     city: 'New York',
   }
 };
 
-const spiderman = structuredClone(ironman);
+const spiderman: Person = {
+  firstName: 'Peter',
+  lastName: 'Parker',
+  age: 22,
+  address: {
+    postalCode: '456',
+    city: 'New York',
+  }
+}
 
-spiderman.firstName = 'Peter';
-spiderman.lastName = 'Parker';
-spiderman.age = 22;
-spiderman.address.city = 'San Jose';
+console.log(ironman);
+console.log(spiderman);
 
-console.log(ironman, spiderman);
+
+// const spiderman = structuredClone(ironman);
+
+// spiderman.firstName = 'Peter';
+// spiderman.lastName = 'Parker';
+// spiderman.age = 22;
+// spiderman.address.city = 'San Jose';
+
+// console.log(ironman, spiderman);
 
