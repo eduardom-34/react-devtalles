4866a74438de59351203f890ec9dfc8fd088c3ce
CesarLinux <eduardomelchor34@gmail.com>
2025-06-09 18:56:38 -0600

    Variables y constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,13 @@
+
+const firstName: string = 'Cesar';
+let lastName = 'Casco';
+
+
+let diceNumber = 5;
+diceNumber = 3;
+
+
+const containsLetterE = lastName.includes('C');
+
+console.log(containsLetterE, diceNumber, firstName, lastName);
+
