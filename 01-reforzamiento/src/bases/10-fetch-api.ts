0c7303a92bb40de2b95462df617ea4ec648782c5
CesarLinux <eduardomelchor34@gmail.com>
2025-06-10 19:32:45 -0600

    Interfaces y optimizacion

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,20 +1,29 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
 
 
 const API_KEY = 'lTPykHIx3O5a38eTPBZ4ovQ89JRHmpxd';
 
-const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
+const myRequest = fetch(
+  `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+);
+
+
+const createImageInsideDOM = ( url: string ) => {
+
+  const imgElement = document.createElement('img');
+  imgElement.src = url;
+  
+  document.body.append(imgElement);
+
+}
 
 
 myRequest.then( (response) => response.json())
-.then( (data) => {
-  console.log(data);
-  const imageUrl = data.data.images.original.url;
-  console.log(imageUrl);
-  
-  const imgElement = document.createElement('img');
-  imgElement.src = imageUrl;
+.then( ({ data }: GiphyRandomResponse ) => {
 
-  document.body.append(imgElement);
+  const imageUrl = data.images.original.url;
+  createImageInsideDOM(imageUrl);
+
 })
 .catch( (err) => {
   console.error(err)
