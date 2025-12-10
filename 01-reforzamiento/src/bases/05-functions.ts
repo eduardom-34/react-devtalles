4bfe35bb0c2ee131674c9ee9ece1431f9ba6b6e8
CesarLinux <eduardomelchor34@gmail.com>
2025-12-10 10:29:43 -0600

    destructuring de objetos

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -2,32 +2,47 @@ function greet(name: string): string {
   return `Hello, ${name}`;
 }
 
-const greet2 = (name: string): string => {
-  return `Hello, ${name}`;
-
-}
+const greet2 = (name: string): string => `Hello, ${name}`;
 
 const message = greet('Goku');
 const message2 = greet2('Vegeta');
 
 console.log(message, message2);
 
-function getUser() {
+interface User {
+  uid: string;
+  username: string;
+}
+
+function getUser(): User {
   return {
     uid: 'ABC123',
     username: 'El_Papi1502'
   };
 }
 
-const getUser2 = () => {
-
-  return {
-    uid: 'ABC123',
-    username: 'El_Papi1502'
-  }
-}
+const getUser2 = () => ({
+  uid: 'ABC123',
+  username: 'El_Papi1502'
+});
 
 const user = getUser();
 const user2 = getUser2();
 console.log(user2);
+
+
+// Funcion de flecha, cuando usarlo, para hacer una callback por ejemplo
+const myNumbers: number[] = [1, 2, 3, 4, 5];
+
+//Aqui con funcion trandicional
+// myNumbers.forEach(function (numero) {
+//   console.log({ numero });
+// });
+
+// Aqui con funcion de flecha
+myNumbers.forEach(numero => console.log({ numero }));
+
+
+
+
 
---
+++ 01-reforzamiento/src/bases/06-obj-destructuring.ts
@@ -0,0 +1,61 @@
+const person = {
+  name: 'Tony',
+  age: 45,
+  key: 'Ironman'
+};
+
+const { age, name: ironmanName, key } = person;
+
+// const name = person.name;
+// const age = person.age;
+// const key = person.key;
+
+console.log({ ironmanName, age, key });
+
+
+interface Hero {
+  name: string;
+  age: number;
+  key: string;
+  rank?: string;
+}
+
+
+const useContext = ({ key, name, age, rank = "sin rango" }: Hero) => {
+
+  return {
+    keyName: key,
+    user: {
+      name,
+      age,
+    },
+    rank: rank
+  };
+};
+
+const {
+  rank,
+  keyName,
+  user,
+  // user: { name }
+} = useContext(person);
+
+const { name } = user;
+
+console.log({ rank, keyName, name });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
