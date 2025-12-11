9d75cc13ec0224320b8ac18be82b6989245a78b7
CesarLinux <eduardomelchor34@gmail.com>
2025-12-10 18:43:44 -0600

    Agrupar pruebas similares

---
+++ 02-first-steps/src/helpers/math.helper.test.ts
@@ -0,0 +1,94 @@
+import { describe, expect, test } from 'vitest';
+import { add, multiply, subtract } from './math.helper';
+
+describe('add', () => {
+
+  test('shoud add two positives number', () => {
+
+    // ! 1. Arrange
+    const a = 1;
+    const b = 2;
+
+    // ! 2. Act
+    const result = add(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(3);
+  });
+
+  test('shoud add two positives number', () => {
+
+    // ! 1. Arrange
+    const a = 1;
+    const b = 2;
+
+    // ! 2. Act
+    const result = add(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(3);
+  });
+
+});
+
+describe('subtract', () => {
+
+  test('shoud subtract two positives number', () => {
+
+    // ! 1. Arrange
+    const a = 6;
+    const b = 2;
+
+    // ! 2. Act
+    const result = subtract(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a - b);
+  });
+
+  test('shoud subtract two negative number', () => {
+
+    // ! 1. Arrange
+    const a = -6;
+    const b = -2;
+
+    // ! 2. Act
+    const result = subtract(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a - b);
+  });
+
+});
+
+
+describe('multiply', () => {
+
+  test('shoud multiply two positives number', () => {
+
+    // ! 1. Arrange
+    const a = 3;
+    const b = 2;
+
+    // ! 2. Act
+    const result = multiply(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a * b);
+  });
+  test('shoud multiply two numbers, one positive and one negative', () => {
+
+    // ! 1. Arrange
+    const a = 3;
+    const b = -2;
+
+    // ! 2. Act
+    const result = multiply(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a * b);
+  });
+});
+
+
+
