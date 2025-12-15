3a942801fdcd600cd81718e861e89d8764acda78
CesarLinux <eduardomelchor34@gmail.com>
2025-12-14 23:06:24 -0600

    Obtener gifs mendiante peticion http

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -4,6 +4,7 @@ import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from './shared/components/SearchBar';
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
 
 export const GifsApp = () => {
 
@@ -13,9 +14,18 @@ export const GifsApp = () => {
     console.log({ term });
   }
 
-  const handleSearch = (query: string) => {
-    console.log({ query });
+  const handleSearch = async (query: string = '') => {
 
+    query = query.trim().toLowerCase();
+    if (query.length === 0) return;
+
+    if (previousTerms.includes(query)) return;
+
+    setPreviousTerms([query, ...previousTerms.slice(0, 7)]);
+
+    const gifs = await getGifsByQuery(query);
+
+    console.log({ gifs })
   }
 
   return (
