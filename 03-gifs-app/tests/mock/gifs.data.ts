0922af19ec0ab11e9f44e57ac49fb19b6ccfe006
CesarLinux <eduardomelchor34@gmail.com>
2025-12-19 22:18:43 -0600

    Pruebas sobre accion - getGifsByQuery

---
+++ 03-gifs-app/tests/mock/gifs.data.ts
@@ -0,0 +1,72 @@
+export const gifsMock = [
+  {
+    id: 'B6SyssSlTgPXq',
+    title: 'Dragonball Goku Super Saiyan GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/B6SyssSlTgPXq/giphy.gif',
+    width: 200,
+    height: 200
+  },
+  {
+    id: 'eJ1U3jkPwvnGTcTiRz',
+    title: 'Dragon Ball Manga Edit GIF',
+    url: 'https://media3.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/eJ1U3jkPwvnGTcTiRz/giphy.gif',
+    width: 360,
+    height: 480
+  },
+  {
+    id: 'o7PX1pWVblbtWn85qh',
+    title: 'Dragonball GIF',
+    url: 'https://media4.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/o7PX1pWVblbtWn85qh/giphy.gif',
+    width: 270,
+    height: 480
+  },
+  {
+    id: 'kvn0FeqyOXgohYNBNa',
+    title: 'Goku GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/kvn0FeqyOXgohYNBNa/giphy.gif',
+    width: 272,
+    height: 480
+  },
+  {
+    id: 'fPaLZFOsXYKh7QFng7',
+    title: 'Goku Vs Luffy GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/fPaLZFOsXYKh7QFng7/giphy.gif',
+    width: 480,
+    height: 480
+  },
+  {
+    id: 'bPLlVHjnlfS6m3wIjg',
+    title: 'Look At You Dragon Ball GIF',
+    url: 'https://media0.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/bPLlVHjnlfS6m3wIjg/giphy.gif',
+    width: 480,
+    height: 480
+  },
+  {
+    id: '28Uh9EJN06mvS',
+    title: 'son goku water GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/28Uh9EJN06mvS/giphy.gif',
+    width: 500,
+    height: 500
+  },
+  {
+    id: 'e2tSgEk1k8Baw',
+    title: 'goku GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/e2tSgEk1k8Baw/giphy.gif',
+    width: 480,
+    height: 270
+  },
+  {
+    id: 'syBjh3ltBqHHLYcsUf',
+    title: 'L Ssg Goku GIF',
+    url: 'https://media4.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/syBjh3ltBqHHLYcsUf/giphy.gif',
+    width: 392,
+    height: 480
+  },
+  {
+    id: 'fWgwBq6OGY1kPjCqDP',
+    title: 'Dragon Ball Super GIF by Funimation',
+    url: 'https://media4.giphy.com/media/v1.Y2lkPTIyNDg2NmJjc2I4dTJrNGhmamlvcXhnMWxwdTljdG53dmR1OGQwemZyZWdwY3d3MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/fWgwBq6OGY1kPjCqDP/giphy.gif',
+    width: 480,
+    height: 268
+  }
+]
