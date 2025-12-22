db61962af989bbdb6818cec3a0a408932e2a7230
CesarLinux <eduardomelchor34@gmail.com>
2025-12-21 21:39:52 -0600

    Pruebas sobre useGifs

---
+++ 03-gifs-app/src/gifs/hooks/useGifs.test.tsx
@@ -0,0 +1,44 @@
+import { renderHook } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { useGifs } from "./useGifs";
+import { act } from "react";
+
+describe("useGifts", () => {
+
+  test('should return default values en methods', () => {
+
+    const { result } = renderHook(() => useGifs());
+
+    expect(result.current.gifs.length).toBe(0);
+    expect(result.current.previousTerms.length).toBe(0);
+    expect(result.current.handleSearch).toBeDefined();
+    expect(result.current.handleTermClicked).toBeDefined();
+
+  });
+
+  test('should return a list of gifs', async () => {
+    // handleSearch
+
+    const { result } = renderHook(() => useGifs());
+
+    await act(async () => {
+      await result.current.handleSearch('goku');
+    });
+
+    expect(result.current.gifs.length).toBe(10);
+
+  });
+
+  test('should return a list of gifs when handleTermClicked is called', async () => {
+
+    const { result } = renderHook(() => useGifs());
+
+    await act(async () => {
+      await result.current.handleTermClicked('goku');
+    });
+
+    expect(result.current.gifs.length).toBe(10);
+
+  });
+
+});
