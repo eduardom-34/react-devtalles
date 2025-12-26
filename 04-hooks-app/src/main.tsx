8deca573cc61e7e79982fa3bd3f9c8818946add2
CesarLinux <eduardomelchor34@gmail.com>
2025-12-25 22:35:35 -0600

    Tarea - Palabras revueltas

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -10,6 +10,7 @@ import './index.css'
 // import { PokemonPage } from './03-examples/PokemonPage';
 // import { FocusScreen } from './useRef/FocusScreen';
 import { TasksApp } from './05-useReducer/TaskApp';
+import { ScrambleWords } from './05-useReducer/ScrambleWords';
 
 
 createRoot(document.getElementById('root')!).render(
@@ -20,7 +21,9 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithHook /> */}
     {/* < PokemonPage /> */}
     {/* < FocusScreen /> */}
+    {/* <TasksApp /> */}
+    <ScrambleWords />
 
-    <TasksApp />
+
   </StrictMode>,
 )
