535d32de2fb5c03d0d4db9a28d2f7fcb0d1390cc
CesarLinux <eduardomelchor34@gmail.com>
2025-12-27 23:29:33 -0600

    Simular fallo en useOptimistic + sonner

--- 04-hooks-app/src/ 07-useOptimistic/InstagromApp.tsx
+++ 04-hooks-app/src/ 07-useOptimistic/InstagromApp.tsx
@@ -1,4 +1,5 @@
 import { useOptimistic, useState, useTransition } from 'react';
+import { toast } from "sonner"
 
 interface Comment {
   id: number;
@@ -37,23 +38,33 @@ export const InstagromApp = () => {
 
     startTransition(async () => {
 
-      await new Promise(resolve => setTimeout(resolve, 3000));
+      await new Promise(resolve => setTimeout(resolve, 1000));
       console.log("Servidor respondió")
 
       console.log('Mensaje grabado');
 
-      setComments(prev => [
-        ...prev,
-        {
-          id: new Date().getTime(),
-          text: messageText,
+      // setComments(prev => [
+      //   ...prev,
+      //   {
+      //     id: new Date().getTime(),
+      //     text: messageText,
+      //   },
+      // ]);
+
+
+      // !Este seria el codigo para revertir el proceso
+      setComments(prev => prev);
+      toast('Error al agregar el comentario', {
+        description: 'Inténtalo de nuevo.',
+        duration: 10_000,
+        position: 'top-right',
+        action: {
+          label: 'Cerrar',
+          onClick: () => toast.dismiss(),
         },
-      ]);
 
-    })
-
-
-
+      })
+    });
 
   };
 
@@ -106,6 +117,7 @@ export const InstagromApp = () => {
           Enviar
         </button>
       </form>
+      {/* <Toaster /> */}
     </div>
   );
 };
--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+// import { Toaster } from 'sonner'
+
 // import { HooksApp } from './HooksApp'
 
 import './index.css'
-import { MemoHook } from './06-memos/MemoHook'
-import { MemoCounter } from './06-memos/MemoCounter'
-import { InstagromApp } from './ 07-useOptimistic/InstagromApp'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { MemoCounter } from './06-memos/MemoCounter'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
@@ -14,10 +15,13 @@ import { InstagromApp } from './ 07-useOptimistic/InstagromApp'
 // import { FocusScreen } from './useRef/FocusScreen';
 // import { TasksApp } from './05-useReducer/TaskApp';
 // import { ScrambleWords } from './05-useReducer/ScrambleWords';
+import { InstagromApp } from './ 07-useOptimistic/InstagromApp'
+import { Toaster } from './components/ui/sonner'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
