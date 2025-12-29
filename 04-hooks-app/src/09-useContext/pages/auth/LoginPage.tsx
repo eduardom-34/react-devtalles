1983e13b31f2f02eae06f5f22e627e778719ef34
CesarLinux <eduardomelchor34@gmail.com>
2025-12-28 21:36:52 -0600

    Navegar entre pantallas - Diseño de páginas

--- 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Link } from "react-router"
+
 export const LoginPage = () => {
   return (
-    <>
-      LoginPage
-    </>
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Iniciar sesion</h1>
+      <hr />
+
+      <form className="flex flex-col gap-2 my-10">
+        <Input type="number" placeholder="ID del usuario" />
+
+        <Button type="submit">Login</Button>
+
+      </form>
+
+      <Link to="/">
+        <Button variant="ghost" className="curso">Volver a la pagina principal</Button>
+      </Link>
+
+    </div >
   )
 }
