02890ed4b3b5f776837ea18236b88ee55868a6b3
CesarLinux <eduardomelchor34@gmail.com>
2026-01-11 00:34:53 -0600

    Mostrar resumen estadístico

--- 05-heroes-app/src/heroes/components/HeroStats.tsx
+++ 05-heroes-app/src/heroes/components/HeroStats.tsx
@@ -1,8 +1,19 @@
+import { useQuery } from "@tanstack/react-query"
+
 import { Badge } from "@/components/ui/badge"
 import { Users, Heart, Zap } from "lucide-react"
 import { HeroStatCard } from "./HeroStatCard"
 
+import { getSummaryAction } from "../actions/get-summary.action"
+
 export const HeroStats = () => {
+
+  const { data: summary } = useQuery({
+    queryKey: ['summary-information'],
+    queryFn: () => getSummaryAction(),
+    staleTime: 1000 * 60 * 5,
+  })
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8">
 
@@ -11,13 +22,15 @@ export const HeroStats = () => {
         title="Total de personajes"
         icon={<Users className="h-4 w-4 text-muted-foreground" />}
       >
-        <div className="text-2xl font-bold">16</div>
+        <div className="text-2xl font-bold">
+          {summary?.totalHeroes}
+        </div>
         <div className="flex gap-1 mt-2">
           <Badge variant="secondary" className="text-xs">
-            12 Heroes
+            {summary?.heroCount} Heroes
           </Badge>
           <Badge variant="destructive" className="text-xs">
-            2 Villains
+            {summary?.villainCount} Villains
           </Badge>
         </div>
       </ HeroStatCard >
@@ -27,6 +40,7 @@ export const HeroStats = () => {
         title="Favoritos"
         icon={<Heart className="h-4 w-4 text-muted-foreground" />}
       >
+        {/* TODO: Tenemos que calcular el porcentaje */}
         <div className="text-2xl font-bold text-red-600">3</div>
         <p className="text-xs text-muted-foreground">18.8% of total</p>
       </ HeroStatCard >
@@ -35,16 +49,22 @@ export const HeroStats = () => {
         title="Fuerte"
         icon={<Zap className="h-4 w-4 text-muted-foreground" />}
       >
-        <div className="text-lg font-bold">Superman</div>
-        <p className="text-xs text-muted-foreground">Strength: 10/10</p>
+        <div className="text-lg font-bold">
+          {summary?.strongestHero.name}
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Strength: {summary?.strongestHero.strength}/10
+        </p>
       </ HeroStatCard >
 
       <HeroStatCard
         title="Inteligente"
         icon={<Heart className="h-4 w-4 text-muted-foreground" />}
       >
-        <div className="text-lg font-bold">Batman</div>
-        <p className="text-xs text-muted-foreground">Intelligence: 10/10</p>
+        <div className="text-lg font-bold">{summary?.smartestHero.name}</div>
+        <p className="text-xs text-muted-foreground">
+          Intelligence: {summary?.smartestHero.intelligence}/10
+        </p>
       </ HeroStatCard >
     </div>
   )
