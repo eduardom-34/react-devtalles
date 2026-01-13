3c53da1420d474526dae1716d7aa7f2852aaa678
CesarLinux <eduardomelchor34@gmail.com>
2026-01-13 08:33:15 -0600

    Página de Héroe

---
+++ 05-heroes-app/src/heroes/actions/get-hero.ts
@@ -0,0 +1,17 @@
+import { heroApi } from "../api/hero.api";
+import type { Hero } from "../types/hero.interface";
+
+
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+
+export const getHero = async (idSlug: string) => {
+
+  const { data } = await heroApi.get<Hero>(`/${idSlug}`);
+
+  return {
+    ...data,
+    image: `${VITE_API_URL}/images/${data.image}`
+
+  }
+
+}
